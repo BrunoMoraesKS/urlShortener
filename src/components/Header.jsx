ef7036c5307a59880ec5f7884e258da3d30e5ddc
BrunoMoraes <brunomoraes.ti@gmail.com>
2021-08-16 08:52:42 -0300

    Finished mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 import logo from "../images/logo.svg";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const openMenu = () => {
+    setIsMenuOpen(true);
+
+    const menu = document.getElementById("menu");
+    menu.classList.add("showMenu");
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+
+    const menu = document.getElementById("menu");
+    menu.classList.remove("showMenu");
+  };
+
   return (
     <header>
       <img src={logo} alt="Logo" />
-      <button>
-        <AiOutlineMenu />
-      </button>
+
+      {isMenuOpen ? (
+        <AiOutlineClose className="menuIcon" onClick={closeMenu} />
+      ) : (
+        <AiOutlineMenu className="menuIcon" onClick={openMenu} />
+      )}
+
+      <nav id="menu">
+        <ul>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Resources</li>
+          <div className="horizontalLine"></div>
+          <li>Login</li>
+          <button className="getStarted">Sign Up</button>
+        </ul>
+      </nav>
     </header>
   );
 };
